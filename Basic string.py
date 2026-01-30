9dc753a9879b7c4dd0e405ab2112c944fa514a32
annasofiaswidorowiczlara <annasofiaswidorowicz@gmail.com>
2026-01-30 11:48:05 +0100

    Basic strings

---
+++ Basic string.py
@@ -0,0 +1,13 @@
+text =  "Hello World"
+print(text)
+text = 'Hello World 2'
+print(text)
+print(text[4]) #prints 4th letter of the text
+print(len(text))
+text = ""
+print(len(text))
+
+text = "Bob"
+print(text[-1]) #prints the last character of the string
+#numbers can be float (decimals) or intergers
+print(text[6//3]) #with // it's interger division
