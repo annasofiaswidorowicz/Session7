6039d0c28ed01539923a3ec941351ffd393bb387
annasofiaswidorowiczlara <annasofiaswidorowicz@gmail.com>
2026-01-30 12:49:05 +0100

    First commit

---
+++ String slicing.py
@@ -0,0 +1,12 @@
+s = "ABCDEFGHIJKLMNOP"
+
+#index is also a very basic slide
+print(s[1])
+print(s[1:3])
+print(s[5:9])
+print(s[:7]) #from start to from position 7
+print(s[4:])
+print(s[:]) #all of it
+print(s[::2]) #in steps of 2
+print(s[:10:3]) #from beginning to position 10 in increments of 3
+print(s[::-1])
